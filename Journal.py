dd590cf2b3c2dba68e470876585d5972d4c2ac33
Chris <prattcmp@comcast.net>
2016-11-24 01:21:59 -0500

    fixed bug that wrote db to the wrong location

--- Journal.py
+++ Journal.py
@@ -1,10 +1,16 @@
 # Module imports
+import os
 from PyQt5 import QtCore
 from peewee import *
 from playhouse.sqlite_ext import SqliteExtDatabase
 from datetime import date
 
-db = SqliteExtDatabase(str(QtCore.QStandardPaths.writableLocation(QtCore.QStandardPaths.AppDataLocation)) + "/OpenJournal.db")
+dbDirectory = str(QtCore.QStandardPaths.writableLocation(QtCore.QStandardPaths.AppDataLocation)) + "/OpenJournal"
+
+if not os.path.exists(dbDirectory):
+    os.makedirs(dbDirectory)
+    
+db = SqliteExtDatabase(dbDirectory + "/OpenJournal.db")
 
 class BaseModel(Model):
     class Meta:
